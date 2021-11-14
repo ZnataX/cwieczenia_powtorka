2294ff4364614130673ced91558f5bd92e77ae19
ZnataX <nata.91@op.pl>
2021-11-14 13:08:08 +0100

    tabelka dwie kolumny

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ public class Main {
          for(int j = 0; j < 6; ++j)
          tabelka1[i][j] = j + 1;
 
+
     for(int i = 0; i < 5; ++i)
         for(int j = 0; j < 6; ++j)
          System.out.println("Kolumna:" + i + ", wiersz: " + j + " ma wartosc: " + tabelka1[i][j]);
